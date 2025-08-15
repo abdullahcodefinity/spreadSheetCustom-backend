495d1548c694c4494d2ab055446bfb2d312189a4
Muhammad Abdullah <muhammad.abdullah@codefinitysol.com>
2025-08-15 15:44:34 +0500

    express parse added

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/sheets', sheetRoutes);
 app.use('/api/sheet-data', sheetDataRoutes);
